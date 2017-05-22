5aa3f1cb7f11515f125a6c6bf02c79cd162250e6
viswanath007 <bloggerfree4u@gmail.com>
2017-05-22 10:47:19 +0000

    Automatic tests

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,7 @@
+var expect      = require("expect");
+
+describe('App', () => {
+    it('should properly run all tests', () => {
+        expect(1).toBe(1);
+    });
+});
---
+++ karma.conf.js
@@ -0,0 +1,26 @@
+var webpackConfig        = require("./webpack.config.js");
+
+module.exports           = function(config){
+    config.set({
+        browsers: ['PhantomJS'],
+        hostname: process.env.IP,
+        port: process.env.PORT,
+        //runnerPort: 0,
+        singleRun: true,
+        frameworks: ['mocha'],
+        files: ['./app/tests/**/*.test.jsx'],
+        preprocessors: {
+            './app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
+        },
+        reporters: ['mocha'],
+        client: {
+            mocha: {
+                timeout: '5000'
+            }
+        },
+        webpack: webpackConfig,
+        webpackServer: {
+            noInfo: true
+        }
+    });
+};
